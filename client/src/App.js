300b5908213fe2ce21a38b5911b77c8feaaf5fe0
Franciscofuhr <franciscofuhr00@gmail.com>
2022-07-27 21:51:24 -0300

    SUP 27/7

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,15 @@ import logo from './logo.svg';
 import './App.css';
 import React from 'react';
 import { Route } from 'react-router-dom';
-import Home from './components/Home';
+import Home from './components/Home/Home';
+import Carrousel from './components/CarrouselExp/carrouselExp';
 
 function App() {
   return (
     <>
-      <Route path='/' component={Home} />
+      <Route  path='/' component={Home} />
+      <Route  path='/' component={Carrousel} />
+
     </>
   );
 }
